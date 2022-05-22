bf6c53c80165edbb6735780b4221ad5b54ee3463
Kisawa16 <kisawa16@Kisawa16.kali.org>
2022-05-22 21:35:51 +0700

    portfolio tailwindcss

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+// hamburger
+const hamburger = document.querySelector('#hamburger');
+const nav = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click',function() {
+    hamburger.classList.toggle('hamburger-active');
+    nav.classList.toggle('hidden');
+
+});
+
+// navbar-fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
