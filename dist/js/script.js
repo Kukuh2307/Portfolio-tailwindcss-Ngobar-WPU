334999868a9b3bee76f8691b0edd1e1dd7df7672
Kukuh2307 <Kukuhagung12@gmail.com>
2022-05-27 19:45:42 -1200

    menambahkan scrooling smoot dan tombol to top

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,24 @@
 // hamburger
-const hamburger = document.querySelector('#hamburger');
-const nav = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector("#nav-menu");
+const toTop = document.querySelector("#to-top");
 
-hamburger.addEventListener('click',function() {
-    hamburger.classList.toggle('hamburger-active');
-    nav.classList.toggle('hidden');
-
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  nav.classList.toggle("hidden");
 });
 
 // navbar-fixed
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
-}
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
