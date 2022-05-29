e59eb4c827cb9c1b39d75aa9b79285fa9ab0fdcc
Kukuh2307 <kukuhagung12@gmail.com>
2022-05-29 12:34:16 +0700

    memperbaiki toggle darkmode dan memperkecil ukuran file css

--- dist/js/script.js
+++ dist/js/script.js
@@ -32,21 +32,23 @@ window.addEventListener('click',function(e){
 });
 
 // darkmode toggle
-const darkToggle = document.querySelector("#dark-toggle");
-const html = document.querySelector("html");
+// Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
 
-darkToggle.addEventListener("click", function () {
+darkToggle.addEventListener('click', function () {
   if (darkToggle.checked) {
-    html.classList.add("dark");
-    localStorage = "dark";
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
   } else {
-    html.classList.remove("dark");
-    localStorage = "light";
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
   }
 });
-// pindahkan posisi toggle sesuai darkmode 
+
+// pindahkan posisi toggle sesuai mode
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   darkToggle.checked = true;
-  } else {
+} else {
   darkToggle.checked = false;
 }
